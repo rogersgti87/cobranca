0a99c935ea5a6b9759eb2a63be799cd6593e68ea
Roger Guimaraes <rogersgti87@gmail.com>
2023-08-12 21:33:42 -0300

    12082023

--- app/Console/Commands/GenerateInvoiceCron.php
+++ app/Console/Commands/GenerateInvoiceCron.php
@@ -26,13 +26,13 @@ class GenerateInvoiceCron extends Command
   {
 
     $sql = "SELECT a.id, a.user_id, c.name customer,c.email,c.email2,c.phone, c.notification_whatsapp, c.company, a.description,a.price, u.access_token_mp,
-	a.gateway_payment,a.payment_method,a.period, CURRENT_DATE date_invoice, DATE_ADD(CONCAT(YEAR(CURRENT_DATE),'-',MONTH(CURRENT_DATE),'-',a.day_due), INTERVAL 0 MONTH) date_due,
-	a.status, 'Pendente',CURRENT_TIMESTAMP created_at,CURRENT_TIMESTAMP updated_at FROM customer_services a
+    a.gateway_payment,a.payment_method,a.period, CURRENT_DATE date_invoice, DATE_ADD(CONCAT(YEAR(CURRENT_DATE),'-',MONTH(CURRENT_DATE),'-',a.day_due), INTERVAL 0 MONTH) date_due,
+    a.status, 'Pendente',CURRENT_TIMESTAMP created_at,CURRENT_TIMESTAMP updated_at FROM customer_services a
     INNER JOIN customers c ON a.customer_id = c.id
     INNER JOIN services s ON a.service_id = s.id
     INNER JOIN users u ON a.user_id = u.id
-    WHERE NOT EXISTS (SELECT * FROM invoices b WHERE a.id = b.customer_service_id AND DATE_ADD(CONCAT(YEAR(CURRENT_DATE),'-',MONTH(CURRENT_DATE),'-',a.day_due), INTERVAL 0 MONTH) = b.date_due)
-    AND CURRENT_DATE = DATE_ADD(DATE_ADD(CONCAT(YEAR(CURRENT_DATE),'-',MONTH(CURRENT_DATE),'-','12'), INTERVAL 0 MONTH), INTERVAL 0 DAY) AND a.status = 'Ativo'";
+    WHERE NOT EXISTS (SELECT * FROM invoices b WHERE a.id = b.customer_service_id AND b.date_due = CURRENT_DATE)
+    AND DATE_ADD(CONCAT(YEAR(CURRENT_DATE),'-',MONTH(CURRENT_DATE),'-',a.day_due), INTERVAL 0 MONTH) =  DATE_ADD(DATE_ADD(CONCAT(YEAR(CURRENT_DATE),'-',MONTH(CURRENT_DATE),'-','12'), INTERVAL 0 MONTH), INTERVAL 0 DAY) AND a.status = 'Ativo'";
 
    $verifyInvoices = DB::select($sql);
 
@@ -40,7 +40,7 @@ class GenerateInvoiceCron extends Command
 
     foreach($verifyInvoices as $vInvoice){
 
-        $invoice = DB::table('invoices')->insert([
+        $newInvoice = DB::table('invoices')->insertGetId([
             'user_id'               => $vInvoice->user_id,
             'customer_service_id'   => $vInvoice->id,
             'description'           => $vInvoice->description,
@@ -55,8 +55,25 @@ class GenerateInvoiceCron extends Command
             'updated_at'            => $vInvoice->updated_at
         ]);
 
-        \Log::info(json_encode($invoice));
-        return 1;
+
+
+        $invoice = Invoice::select('invoices.id','invoices.status','invoices.user_id','invoices.date_invoice','invoices.date_due','invoices.description',
+        'customers.email','customers.email2','customers.phone','customers.whatsapp','customers.name','customers.notification_whatsapp',
+        'customers.company','customers.document','customers.phone','customers.address','customers.number','customers.complement',
+        'customers.district','customers.city','customers.state','customers.cep','invoices.gateway_payment','invoices.payment_method',
+        'services.id as service_id','services.name as service_name','invoices.price','users.access_token_mp','users.company as user_company',
+        'users.whatsapp as user_whatsapp','users.image as user_image', 'users.telephone as user_telephone', 'users.email as user_email',
+        'users.api_access_token_whatsapp','users.token_paghiper','users.key_paghiper',
+        'invoices.image_url_pix','invoices.pix_digitable','invoices.qrcode_pix_base64','invoices.billet_digitable','invoices.billet_base64','invoices.billet_url',
+        DB::raw("DATEDIFF (invoices.date_due,invoices.date_invoice) as days_due_date"))
+        ->join('customer_services','invoices.customer_service_id','customer_services.id')
+        ->join('customers','customer_services.customer_id','customers.id')
+        ->join('services','customer_services.service_id','services.id')
+        ->join('users','users.id','invoices.user_id')
+        ->where('invoices.id',$newInvoice)
+        ->where('invoices.user_id',$vInvoice->user_id)
+        ->first();
+
 
         if($invoice->payment_method == 'Pix'){
             if($invoice->gateway_payment == 'Pag Hiper'){

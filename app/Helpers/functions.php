d609306e39b8f6e3cd70a1b6f94c1ed158143614
Roger Guimaraes <rogersgti87@gmail.com>
2023-08-21 14:14:57 -0300

    21082023

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -18,8 +18,8 @@ function whatsappBold($string) {
 
 function removeEspeciais($get_valor) {
 
-    $source = array('.', ',',' ','(',')','-');
-    $replace = array('', '','','','','');
+    $source = array('.', ',',' ','(',')','-','/');
+    $replace = array('', '','','','','','');
     $valor = str_replace($source, $replace, $get_valor); //remove os pontos e substitui a virgula pelo ponto
     return $valor; //retorna o valor formatado para gravar no banco
 }

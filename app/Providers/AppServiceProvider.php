d95b91c8d3a9d49f609e90875db92dbf87be63a8
Roger Guimaraes <rogersgti87@gmail.com>
2023-09-08 11:32:36 -0300

    08092023

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,9 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        if (config('app.env') === 'production') {
-            \URL::forceSchema('https');
-        }
+
 
     }
 }

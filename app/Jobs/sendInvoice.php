c41401b8fe1b2454375f4eb6ac22e0be622e9dcb
Roger Guimaraes <rogersgti87@gmail.com>
2023-09-20 13:56:15 -0300

    20092023

--- app/Jobs/sendInvoice.php
+++ app/Jobs/sendInvoice.php
@@ -307,12 +307,13 @@ class sendInvoice implements ShouldQueue
     ];
 
 
-    $details['body']  = view('mails.invoice',$details)->render();
+
 
     \Log::info('Linha 312 send_generate_invoice : '.$getInvoice->send_generate_invoice);
     if($getInvoice->send_generate_invoice == 'Sim'){
         \Log::info('Linha 314 notification_email: '.$getInvoice->notification_email);
         if($getInvoice->notification_email == 's'){
+            $details['body']  = view('mails.invoice',$details)->render();
             InvoiceNotification::Email($details);
         }
         if($getInvoice->notification_whatsapp == 's'){

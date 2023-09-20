625b1a67e98ff91869e486fb3179307fb2c00cf9
Roger Guimaraes <rogersgti87@gmail.com>
2023-09-20 14:29:13 -0300

    20092023

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('invoicejob:cron')->everyMinute();
+        //$schedule->command('invoicejob:cron')->everyMinute();
         //$schedule->command('generateinvoice:cron')->everyMinute();
         $schedule->command('rememberinvoice:cron')->twiceDaily(9, 14);
 

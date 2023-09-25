5eed8f869d9940c157cc9f857c64ea0beebfd844
Roger Guimaraes <rogersgti87@gmail.com>
2023-09-25 10:11:15 -0300

    25092023

--- app/Console/Commands/GenerateInvoiceCron.php
+++ app/Console/Commands/GenerateInvoiceCron.php
@@ -84,7 +84,7 @@ a.id, a.user_id, c.name customer,c.email,c.email2,c.phone, c.notification_whatsa
                 'customers.district','customers.city','customers.state','customers.cep','invoices.gateway_payment','invoices.payment_method',
                 'services.id as service_id','services.name as service_name','invoices.price','users.access_token_mp','users.company as user_company',
                 'users.whatsapp as user_whatsapp','users.image as user_image', 'users.telephone as user_telephone', 'users.email as user_email','users.send_generate_invoice',
-                'users.api_access_token_whatsapp','users.token_paghiper','users.key_paghiper','invoices.image_url_pix','invoices.pix_digitable',
+                'users.api_access_token_whatsapp','users.token_paghiper','users.key_paghiper','invoices.image_url_pix','invoices.pix_digitable','invoices.transaction_id',
                 'invoices.qrcode_pix_base64','invoices.billet_digitable','invoices.billet_base64','invoices.billet_url','users.inter_chave_pix',
                 'users.inter_host','users.inter_client_id','users.inter_client_secret','users.inter_scope','users.inter_crt_file','users.inter_key_file','users.inter_crt_file_webhook',
                 DB::raw("DATEDIFF (invoices.date_due,invoices.date_invoice) as days_due_date"))
@@ -240,7 +240,7 @@ a.id, a.user_id, c.name customer,c.email,c.email2,c.phone, c.notification_whatsa
                                 'customers.district','customers.city','customers.state','customers.cep','invoices.gateway_payment','invoices.payment_method',
                                 'services.id as service_id','services.name as service_name','invoices.price','users.access_token_mp','users.company as user_company',
                                 'users.whatsapp as user_whatsapp','users.image as user_image', 'users.telephone as user_telephone', 'users.email as user_email',
-                                'users.api_access_token_whatsapp','users.token_paghiper','users.key_paghiper','invoices.image_url_pix','invoices.pix_digitable',
+                                'users.api_access_token_whatsapp','users.token_paghiper','users.key_paghiper','invoices.image_url_pix','invoices.pix_digitable','invoices.transaction_id',
                                 'invoices.qrcode_pix_base64','invoices.billet_digitable','invoices.billet_base64','invoices.billet_url','users.inter_chave_pix',
                                 'users.inter_host','users.inter_client_id','users.inter_client_secret','users.inter_scope','users.inter_crt_file','users.inter_key_file','users.inter_crt_file_webhook',
                                 DB::raw("DATEDIFF (invoices.date_due,invoices.date_invoice) as days_due_date"))
@@ -283,7 +283,7 @@ a.id, a.user_id, c.name customer,c.email,c.email2,c.phone, c.notification_whatsa
             'customers.district','customers.city','customers.state','customers.cep','invoices.gateway_payment','invoices.payment_method',
             'services.id as service_id','services.name as service_name','invoices.price','users.access_token_mp','users.company as user_company',
             'users.whatsapp as user_whatsapp','users.image as user_image', 'users.telephone as user_telephone', 'users.email as user_email','users.send_generate_invoice',
-            'users.api_access_token_whatsapp','users.token_paghiper','users.key_paghiper','invoices.image_url_pix','invoices.pix_digitable',
+            'users.api_access_token_whatsapp','users.token_paghiper','users.key_paghiper','invoices.image_url_pix','invoices.pix_digitable','invoices.transaction_id',
             'invoices.qrcode_pix_base64','invoices.billet_digitable','invoices.billet_base64','invoices.billet_url','users.inter_chave_pix',
             'users.inter_host','users.inter_client_id','users.inter_client_secret','users.inter_scope','users.inter_crt_file','users.inter_key_file','users.inter_crt_file_webhook',
             DB::raw("DATEDIFF (invoices.date_due,invoices.date_invoice) as days_due_date"))

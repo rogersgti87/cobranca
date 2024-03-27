3ecd8cfff67267a507c6db901e85bb016f4d4fac
Roger Guimaraes <rogersgti87@gmail.com>
2024-03-27 17:11:41 -0300

    270324

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -36,17 +36,6 @@ class AdminController extends Controller
 
     public function index(){
 
-        if (!Storage::disk('public')->exists('pix')) {
-            Storage::disk('public')->makeDirectory('pix');
-        }
-
-        $pix = '00020101021226980014BR.GOV.BCB.PIX2576spi-qrcode.bancointer.com.br/spi/pj/v2/cobv/2b8c582d50c14fc7adf81ea2063cc4705204000053039865406100.005802BR5901*6013SAO PEDRO DA 61082894946462070503***630463EE';
-
-        $invoice['user_id'] = 1;
-        $invoice['id'] = 1;
-
-        QrCode::format('png')->size(220)->generate($pix, storage_path('app/public'). '/pix/' . $invoice['user_id'].'_'.$invoice['id'].'.'.'png');
-
 
     //     $user = User::where('id',3)->first();
     //     $access_token = $user['access_token_inter'];
